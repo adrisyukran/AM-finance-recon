11b81539a64151cb94780bbcdbf7d74ce0f43cb3
Adri Syukran Saadon <adri.syukran17@gmail.com>
2026-01-13 12:13:54 +0800

    update port

--- app.py
+++ app.py
@@ -653,4 +653,4 @@ def internal_error(error):
 # ==================== MAIN ====================
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5003)
